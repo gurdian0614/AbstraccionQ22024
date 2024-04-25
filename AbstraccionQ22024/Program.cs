a9228f303b55a0b329d42f25f3e152ca8bc93ab2
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-24 19:14:45 -0600

    Implementando clase abstracta

--- AbstraccionQ22024/Program.cs
+++ AbstraccionQ22024/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AbstraccionQ22024;
+
+//Creando un objeto
+Vertebrado vertebrado = new Vertebrado();
+vertebrado.Nombre = "Perro";
+vertebrado.Color = "Blanco";
+vertebrado.Tamano = "Pequeño";
+vertebrado.Familia = "Husky";
+vertebrado.Tipo = "Siberiano";
+vertebrado.EsDomestico = true;
+vertebrado.EsHerbivoro = false;
+vertebrado.Imprimir();
