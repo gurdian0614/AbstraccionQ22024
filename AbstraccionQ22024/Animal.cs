733173095b2d13623c68ef0f138c57f555efce88
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-24 19:07:49 -0600

    Add project files.

---
+++ AbstraccionQ22024/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraccionQ22024
+{
+    public abstract class Animal
+    {
+        /// <summary>
+        /// Nombre del Animal
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Color del Animal
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Tamaño del Animal
+        /// </summary>
+        public string Tamano { get; set; }
+
+        /// <summary>
+        /// Familia del Animal
+        /// </summary>
+        public String Familia { get; set; }
+
+        /// <summary>
+        /// Método abstracto para visualizar datos
+        /// </summary>
+        public abstract void Imprimir();
+
+    }
+}
---
+++ AbstraccionQ22024/Vertebrado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraccionQ22024
+{
+    public class Vertebrado : Animal
+    {
+        
+        public override void Imprimir()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
